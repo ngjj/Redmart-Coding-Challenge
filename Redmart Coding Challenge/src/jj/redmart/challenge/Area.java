4b229e01c5a13f3f18eac91fdf203e536026746c
Jun Jie <ngjj@hotmail.sg>
2015-12-03 01:24:29 +0800

    initial commit for redmart challenge one

---
+++ Redmart Coding Challenge/src/jj/redmart/challenge/Area.java
@@ -0,0 +1,31 @@
+package jj.redmart.challenge;
+
+public class Area {
+
+	private int elevation;
+	private boolean visited;
+	
+	
+
+	public Area(int elevation) {
+		this.elevation = elevation;
+		this.visited = false;
+	}
+
+	public int getElevation() {
+		return elevation;
+	}
+
+	public void setElevation(int elevation) {
+		this.elevation = elevation;
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+}
